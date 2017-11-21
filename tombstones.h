d4bf7058ea6ff3a58e86bd68e0724353217f0d6e
Jordy Guntur <jguntur@cycle1.csug.rochester.edu>
2017-11-21 18:38:06 -0500

    7/8 test cases

--- tombstones.h
+++ tombstones.h
@@ -46,13 +46,14 @@ public:
     // Default constructor
     Pointer<T>() : tStone()
     {    
-        // std::cout << "Default Constructor" << std::endl;
+        std::cout << "Default Constructor" << std::endl;
     }
     
     // Copy constructor
     Pointer<T>(Pointer<T>& rhs) : tStone(rhs.tStone)
     {        
-        // std::cout << "Copy Constructor" << std::endl;
+        std::cout << "Copy Constructor" << std::endl;
+        tStone->refCount = 0;
         tStone->addRef();
         // std::cout << tStone << " = " << rhs.tStone << std::endl;
     }
@@ -60,7 +61,7 @@ public:
     // Boostrapping Constructor
     Pointer<T>(T* pVal)
     {
-        // std::cout << "Boostrapping Constructor" << std::endl;
+        std::cout << "Boostrapping Constructor" << std::endl;
         tStone = new Tombstone<T>();
         tStone->pointee = pVal;
         tStone->addRef();
@@ -70,32 +71,38 @@ public:
     // Destroy a pointer
     ~Pointer<T>()
     {
-        // std::cout << "Destructor" << std::endl;
+        std::cout << "Destructor" << std::endl;
+        std::cout << "Reference Count: " << tStone->refCount << std::endl;
+
         tStone->release();
-        // std::cout << tStone->refCount << std::endl;
-        if(tStone->refCount <= 0) {
-            delete tStone;
+        
+        if(tStone->refCount == 0 && tStone->pointee != NULL) {
+            std::cout << "######################################################" << std::endl;
+            std::cout << "Error: Memory Leak." << std::endl;
+            std::cout << "######################################################" << std::endl;
+            // tStone = NULL;
+            exit(-1);
         }
     }
 
     // Dereferencer
     T& operator*() const
     {
-        // std::cout << "Dereference" << std::endl;
+        std::cout << "Dereference" << std::endl;
         return *tStone->pointee;
     }
 
     // Field Dereferencer
     T* operator->() const
     {
-        // std::cout << "Field Dereferencer" << std::endl;
+        std::cout << "Field Dereferencer" << std::endl;
         return tStone->pointee;
     }
 
     // Assignment to our pointer
     Pointer<T>& operator=(const Pointer<T>& rhs)
     {   
-        // std::cout << "Assignment" << std::endl;    
+        std::cout << "Assignment" << std::endl;    
         tStone = rhs.tStone;
         tStone->addRef();
         return *this;
@@ -104,10 +111,16 @@ public:
     // Delete the object that is being pointed at
     friend void free(Pointer<T>& rhs)
     {
-        // std::cout << "Free" << std::endl;
-        // std::cout << rhs.tStone->pointee << std::endl;
+        std::cout << "Free" << std::endl;
 
-        if(rhs.tStone->isFree == true) {
+        if(rhs == 0) {
+            std::cout << "######################################################" << std::endl;
+            std::cout << "Error: Releasing uninitialized pointer. Exiting program." << std::endl;
+            std::cout << "######################################################" << std::endl;
+            exit(-1);
+        }
+        
+        if(rhs.tStone->isFree == true)   {
             std::cout << "######################################################" << std::endl;
             std::cout << "Error: Pointer already freed. Exiting program." << std::endl;
             std::cout << "######################################################" << std::endl;
@@ -115,10 +128,13 @@ public:
         }
         rhs.tStone->isFree = true;
         
-        if(rhs.tStone->refCount == 0 && rhs.tStone) {
-            delete rhs.tStone->pointee;
-            delete rhs.tStone;
-        }
+        // if(rhs.tStone->refCount == 0 && rhs.tStone) {
+        
+        // }
+
+        delete(rhs.tStone->pointee);
+        rhs.tStone->pointee = NULL;
+        rhs = NULL;
     }
     
     bool operator==(const Pointer<T>& ptrIn) const
