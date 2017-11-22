73870cc3705a1d06c3f8902f90b92271b90a04c2
Jordy Guntur <jguntur@cycle3.csug.rochester.edu>
2017-11-22 09:44:13 -0500

    commented code

--- tombstones.h
+++ tombstones.h
@@ -2,6 +2,16 @@
 // tombstones.h, expected interface for CSC254 assignment 5
 /////////////////////////////////////////////////////////////////////////////
 
+/*
+--------------------------------------------------------------------------------------
+University of Rochester
+CSC 254 -- Fall 2017
+Assignment 05: Tombstones
+Jordy Guntur, Marcelo Gheiler
+--------------------------------------------------------------------------------------
+*/
+
+
 #if !defined(__TOMBSTONES_H__)
 #define __TOMBSTONES_H__
 
@@ -11,76 +21,75 @@
 template <class T> class Pointer;
 template <class T> void free(Pointer<T>& obj);
 
+// Class to represent a tombstone
 template <class T> class Tombstone
 {
 public:
-    int refCount;
-    T* pointee;
-    bool isFree;
+    int refCount;       // Reference Count
+    T* pointee;         // Pointer to object
+    bool isFree;        // Whether tombstone has beem freed or not
 
-    Tombstone<T>() : pointee(0), refCount(0) 
-    {
-        isFree = false;
-    }
-    Tombstone<T>(T* ptrIn) : pointee(ptrIn), refCount(1)
-    {
-        isFree = false;
-    }
+    Tombstone<T>() : pointee(0), refCount(0), isFree(false) {}                  // Default Constructor
+    Tombstone<T>(T* ptrIn) : pointee(ptrIn), refCount(1), isFree(false) {}      // Boostrapping Constructor
 
     void addRef() {
         refCount++;
     }
+
     void release() {
         refCount--;
     }
+    
     int refCountCheck() {
         return refCount;
     }
 };
 
+// Pointer Class
 template <class T> class Pointer 
 {
 private:
     Tombstone<T>* tStone;
 public:
+    
     // Default constructor
     Pointer<T>() : tStone()
     {    
-        std::cout << "Default Constructor" << std::endl;
+        // std::cout << "Default Constructor" << std::endl;
     }
     
     // Copy constructor
     Pointer<T>(Pointer<T>& rhs) : tStone(rhs.tStone)
     {        
-        std::cout << "Copy Constructor" << std::endl;
+        // std::cout << "Copy Constructor" << std::endl;
         tStone->refCount = 0;
         tStone->addRef();
-        // std::cout << tStone << " = " << rhs.tStone << std::endl;
     }
 
     // Boostrapping Constructor
     Pointer<T>(T* pVal)
     {
-        std::cout << "Boostrapping Constructor" << std::endl;
+        // std::cout << "Boostrapping Constructor" << std::endl;
+
         tStone = new Tombstone<T>();
         tStone->pointee = pVal;
         tStone->addRef();
-        // std::cout << tStone->refCount << std::endl;
     }
 
     // Destroy a pointer
     ~Pointer<T>()
     {
-        std::cout << "Destructor" << std::endl;
-        std::cout << "Reference Count: " << tStone->refCount << std::endl;
+        // std::cout << "Destructor" << std::endl;
+        // std::cout << "Reference Count: " << tStone->refCount << std::endl;
 
-        tStone->release();
+        // Decrement the reference count
+        tStone->release();          
         
+        // If a pointer is being destroyed, the ref count is 0, and the pointee is not null, it is a memory leak
         if(tStone->refCount == 0 && tStone->pointee != NULL) {
             std::cout << "######################################################" << std::endl;
             std::cout << "Error: Memory Leak." << std::endl;
             std::cout << "######################################################" << std::endl;
-            // tStone = NULL;
             exit(-1);
         }
     }
@@ -88,7 +97,9 @@ public:
     // Dereferencer
     T& operator*() const
     {
-        std::cout << "Dereference" << std::endl;
+        // std::cout << "Dereferencer" << std::endl;
+
+        // If the pointee is null while dereferencing, it is a dangling pointer
         if(tStone->pointee == NULL) {
             std::cout << "######################################################" << std::endl;
             std::cout << "Error: Dangling Pointer Exception." << std::endl;
@@ -102,7 +113,7 @@ public:
     // Field Dereferencer
     T* operator->() const
     {
-        std::cout << "Field Dereferencer" << std::endl;
+        // std::cout << "Field Dereferencer" << std::endl;
         if(tStone->pointee == NULL) {
             std::cout << "######################################################" << std::endl;
             std::cout << "Error: Dangling Pointer Exception." << std::endl;
@@ -117,7 +128,7 @@ public:
     // Assignment to our pointer
     Pointer<T>& operator=(const Pointer<T>& rhs)
     {   
-        std::cout << "Assignment" << std::endl;    
+        // std::cout << "Assignment" << std::endl;    
         tStone = rhs.tStone;
         tStone->addRef();
         return *this;
@@ -126,8 +137,9 @@ public:
     // Delete the object that is being pointed at
     friend void free(Pointer<T>& rhs)
     {
-        std::cout << "Free" << std::endl;
+        // std::cout << "Free" << std::endl;
 
+        // if the pointer is null when freeing, we know it is uninitialized
         if(rhs == 0) {
             std::cout << "######################################################" << std::endl;
             std::cout << "Error: Releasing uninitialized pointer. Exiting program." << std::endl;
@@ -135,6 +147,7 @@ public:
             exit(-1);
         }
         
+        // This checks whether the pointer has already freed
         if(rhs.tStone->isFree == true)   {
             std::cout << "######################################################" << std::endl;
             std::cout << "Error: Pointer already freed. Exiting program." << std::endl;
@@ -142,10 +155,6 @@ public:
             exit(-1);
         }
         rhs.tStone->isFree = true;
-        
-        // if(rhs.tStone->refCount == 0 && rhs.tStone) {
-        
-        // }
 
         delete(rhs.tStone->pointee);
         rhs.tStone->pointee = NULL;
@@ -168,26 +177,17 @@ public:
 
     bool operator==(const int number) const
     {
-        return (number == 0) && (tStone == 0);
         // true iff Pointer is null and int is zero
+        return (number == 0) && (tStone == 0);
+        
     }
     
     bool operator!=(const int number) const
     {
-        return !(number == 0);
         // false iff Pointer is null and int is zero    
+        return !(number == 0);
+        
     }
-    
-    // bool operator==(const int n, const Pointer<T>& t) 
-    // { 
-    //     return t == n; 
-    // }
-
-    // bool operator!=(const int n, const Pointer<T>& t) 
-    // {
-    //     return t != n; 
-    // }
-    
 };
 
 
